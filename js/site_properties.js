e7b8ad69272f7efe6b0e550993d81272ba1c9026
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-02-04 14:06:22 +0530

    Added UC1-UC2-UC3-Add-GET-POST-To-JSON performed GET and POST method

---
+++ js/site_properties.js
@@ -0,0 +1,6 @@
+let site_properties = {
+    home: "../html/Home.html",
+    addPerson: "../html/AddPersonAddressBook.html",
+    use_local_storage: "false",
+    server_url: "http://localhost:3000/bookDB/"
+}
