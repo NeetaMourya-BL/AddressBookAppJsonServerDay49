099e385b70453fc6dcdecfd0526525d0636979c6
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-02-04 14:26:46 +0530

    [neeta mourya] refector for check all function

--- js/site_properties.js
+++ js/site_properties.js
@@ -2,5 +2,5 @@ let site_properties = {
     home: "../html/Home.html",
     addPerson: "../html/AddPersonAddressBook.html",
     use_local_storage: "false",
-    server_url: "http://localhost:3000/bookDB/"
+    server_url: "http://localhost:3000/addressbookappDB/"
 }
